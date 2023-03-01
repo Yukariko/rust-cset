4e341cc1aca6eee42e7c9b429a9e8a9b5c3fd8da
yukariko <yukariko@github.com>
2023-03-01 18:57:37 +0900

    Add a base of command line parser

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use clap::{Arg, ArgAction, ArgMatches, Command, arg};
+
+pub fn app() -> Command {
+    Command::new("cset")
+        .subcommand(
+            Command::new("proc")
+                .arg(arg!(-l --list "list"))
+        )
+        .subcommand(
+            Command::new("set")
+                .arg(arg!(-l --list "list"))
+        )
+}
+
 fn main() {
-    println!("Hello, world!");
+    let matches = app().get_matches();
 }
