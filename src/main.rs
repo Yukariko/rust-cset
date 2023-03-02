5f18e7b914e6225b8982685e6b4056ef074bcdf0
yukariko <yukariko@github.com>
2023-03-02 23:13:11 +0900

    Get cpuset entries

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
 use clap::{Arg, ArgAction, ArgMatches, Command, arg};
+use std::fs;
+
+const CSET_PATH: &str = "/sys/fs/cgroup/cpuset";
 
 pub fn app() -> Command {
     Command::new("cset")
@@ -12,19 +15,29 @@ pub fn app() -> Command {
         )
 }
 
-fn do_proc(matches : &ArgMatches) {
+fn do_proc(matches : &ArgMatches) -> std::io::Result<()> {
     println!("proc");
+    Ok(())
 }
 
-fn do_set(matches : &ArgMatches) {
-    println!("set");
+fn do_set(matches : &ArgMatches) -> std::io::Result<()> {
+    for dir in fs::read_dir(CSET_PATH)? {
+        let dir = dir?;
+
+        if dir.path().is_dir() {
+            if let Some(path) = dir.file_name().to_str() {
+                println!("{}", path);
+            }
+        }
+    }
+    Ok(())
 }
 
 fn main() {
     let matches = app().get_matches();
-    match matches.subcommand() {
+    let results = match matches.subcommand() {
         Some(("proc", sub_matches)) => do_proc(sub_matches),
         Some(("set", sub_matches)) => do_set(sub_matches),
-        _ => println!("none"),
-    }
+        _ => Ok(()),
+    };
 }
