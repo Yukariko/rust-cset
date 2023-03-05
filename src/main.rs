90c66cbf37f921021fc8f7d0008d8bcd50986d3b
yukariko <yukariko@github.com>
2023-03-05 16:13:48 +0900

    Support a --recursive option

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
+use std::io;
 use clap::{Arg, ArgAction, ArgMatches, Command, arg};
-use std::fs;
+use std::fs::{self, DirEntry};
+use std::path::Path;
 
 const CSET_PATH: &str = "/sys/fs/cgroup/cpuset";
 
@@ -12,23 +14,48 @@ pub fn app() -> Command {
         .subcommand(
             Command::new("set")
                 .arg(arg!(-l --list "list"))
+                .arg(arg!(-r --recursive "recursive"))
         )
 }
 
-fn do_proc(matches : &ArgMatches) -> std::io::Result<()> {
+fn visit_dirs(dir: &Path, cb: &dyn Fn(&DirEntry)) -> io::Result<()> {
+    for entry in fs::read_dir(dir)? {
+        let entry = entry?;
+        let path = entry.path();
+        if path.is_dir() {
+            cb(&entry);
+            visit_dirs(&path, cb)?;
+        }
+    }
+    Ok(())
+}
+
+fn enter_dirs(path : &str, cb : &dyn Fn(&DirEntry), recursive : bool) -> io::Result<()> {
+    let path = CSET_PATH.to_owned() + path;
+    for dir in fs::read_dir(path)? {
+        let dir = dir?;
+        let path = dir.path();
+
+        if path.is_dir() {
+            cb(&dir);
+            if recursive {
+                visit_dirs(&path, cb);
+            }
+        }
+    }
+    Ok(())
+}
+
+fn do_proc(matches : &ArgMatches) -> io::Result<()> {
     println!("proc");
     Ok(())
 }
 
-fn do_set(matches : &ArgMatches) -> std::io::Result<()> {
-    for dir in fs::read_dir(CSET_PATH)? {
-        let dir = dir?;
-
-        if dir.path().is_dir() {
-            if let Some(path) = dir.file_name().to_str() {
-                println!("{}", path);
-            }
-        }
+fn do_set(matches : &ArgMatches) -> io::Result<()> {
+    if matches.get_flag("list") {
+        println!("list");
+        enter_dirs("/", &|e| println!("{:?}", e), matches.get_flag("recursive"));
+        return Ok(())
     }
     Ok(())
 }
